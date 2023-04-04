4a1a8a3551405faa055b3be9ffe6b85a8d1c4d6d
Bob-the-Bobothy <bobothyinc@gmail.com>
2023-04-03 22:36:34 -0700

    rebuilt it to actually work I think

--- setup.py
+++ setup.py
@@ -2,13 +2,10 @@ from setuptools import setup
 
 setup(
     name="bobothy-colors",
-    version="1.0.2",
+    version="1.0.3",
     description="library of rgb colors",
     py_modules=["colors"],
     python_requires=">=2.0, >=3.0",
     author="Bob-the-Bobothy",
     url="https://github.com/Bob-the-Bobothy/colors",
-    install_requires=[
-        "random",
-    ],
 )
